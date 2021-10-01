0eb4009f9949d884241900dc7d12f9308417df06
guaaan <juampablo.txt@gmail.com>
2021-10-01 16:32:41 -0300

    enviar cambios

--- src/App.js
+++ src/App.js
@@ -4,53 +4,59 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Select from 'react-select'
 
 function App() {
-  const [usuarios, setUsuarios] = useState([]);
-  const [tablaUsuarios, setTablaUsuarios] = useState([]);
-  const [busqueda, setBusqueda] = useState("");
 
+  const [Medicamentos, setMedicamentos] = useState([]);
+  const [tablaMedicamentos, setTablaMedicamentos] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [refSet, setRefSet] = useState([]);
+
+  
+  
   const peticionGet = async () => {
     await axios.get("https://jsonplaceholder.typicode.com/users")
-      .then(response => {
-        setUsuarios(response.data);
-        setTablaUsuarios(response.data);
+    .then(response => {
+      setMedicamentos(response.data);
+      setTablaMedicamentos(response.data);
       }).catch(error => {
         console.log(error);
       })
-  }
+    }
 
-  const handleChange = e => {
-    setBusqueda(e.target.value);
-    filtrar(e.target.value);
-  }
-
-  const filtrar=(terminoBusqueda)=>{
-    var resultadosBusqueda=tablaUsuarios.filter((elemento)=>{
-      if(elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
-      || elemento.company.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
-      ){
-        return elemento;
-      }
-    });
-    setUsuarios(resultadosBusqueda);
-  }
-
-  useEffect(() => {
-    peticionGet();    
-  }, [])
-
-  return (
-    <div className="App">
+    const handleChange = e => {
+      setBusqueda(e.target.value);
+      filtrar(e.target.value);
+    }
+    
+    const filtrar = (terminoBusqueda) => {
+      var resultadosBusqueda = tablaMedicamentos.filter((elemento) => {
+        if (elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+        || elemento.company.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+        ) {
+          return elemento;
+        }
+      });
+      setMedicamentos(resultadosBusqueda);
+    }
+    
+    useEffect(() => {
+      peticionGet();
+    }, [])
+    
+    
+    return (
+      <div className="App">
       <div className="containerInput">
-        
+
         <input
           className="form-control inputBuscar"
           value={busqueda}
           placeholder="Nombre del medicamento (Mínimo 2 caracteres)"
           onChange={handleChange}
-        />
-        <select className="form-control inputBuscar" value={busqueda} placeholder="Búsqueda por Nombre o Empresa" onChange={handleChange}>
+          />
+        <select className="form-control inputBuscar" value={refSet} placeholder="Búsqueda por Nombre o Empresa" onChange={handleChange} >
           <option value="1002">Petitorio Mínimo Farmacéutico</option>
           <option value="1003">Ley Ricarte Soto</option>
           <option value="1004">Psicotrópicos</option>
@@ -68,7 +74,7 @@ function App() {
             <tr>
               <th>ID</th>
               <th>Nombre</th>
-              <th>Teléfono</th>
+              <th>Status</th>
               <th>Nombre de Usuario</th>
               <th>Correo</th>
               <th>Sitio Web</th>
@@ -77,8 +83,8 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {usuarios &&
-              usuarios.map((usuario) => (
+            {Medicamentos &&
+              Medicamentos.map((usuario) => (
                 <tr key={usuario.id}>
                   <td>{usuario.id}</td>
                   <td>{usuario.name}</td>
