7fc1f6949d5efd8e3bd454e098cbcc0cdb0c6023
guaaan <juampablo.txt@gmail.com>
2021-10-01 14:17:41 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Formulario from './components/Formulario.Js';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      holamundo
+
+      
+    </div>
+  );
+}
+
+export default App;
