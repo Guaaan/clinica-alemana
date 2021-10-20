41062ef33d1afc0d582ae26aa17f6673b7046006
guaaan <juampablo.txt@gmail.com>
2021-10-20 12:31:06 -0300

    funciona

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,13 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 
 function List({ medicdata }) {
   return (
     <Fragment>
       <h1>lista de Medicamentos</h1>
       <ul>
-          {medicdata.map((medicamento, index) => {
-              return <li key={index}>{medicamento.term}</li>
-          })}
+        {medicdata.map((medicamento, index) => {
+          return <li key={index}>{medicamento.term}</li>;
+        })}
       </ul>
     </Fragment>
   );
--- src/containers/MedicListContainer.js
+++ src/containers/MedicListContainer.js
@@ -4,42 +4,33 @@ import List from "../components/List";
 
 class MedicListContainer extends Component {
   state = {
-    medicData: [],
-  };
+    medicData: []
+  }
 
   componentDidMount() {
     //consulta get axios con headers
-    const peticionGet = async () => {
-      axios
-        .get(
+      axios.get(
           "https://gtw-prod.alemana.io/public/api-farmacos/v1/vmpp?q=amoxi&refset=1002",
           {
-            headers: {
-              "X-API-Key":
-                "5e1dc5891a05f000011faf522daf1a4785e04b3ab708d2da1fc7ea4c",
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then((res) => {
-          const medicData = res.data.vmpp;
+            headers: {"X-API-Key": "5e1dc5891a05f000011faf522daf1a4785e04b3ab708d2da1fc7ea4c"},})
+        .then(res => {
+          const medicData = res.data._embedded.vmpp;
 
           this.setState({
-            medicData: medicData,
+            medicData
           });
         })
         .catch((error) => {
           console.log(error);
         });
-    };
+    
   }
   render() {
     const { medicData } = this.state;
-
     return (
-      <>
+      
         <List medicdata={ medicData } />
-      </>
+      
     );
   }
 }
